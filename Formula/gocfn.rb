64ba03a172f134b014b28b876689de4706e32394
goreleaserbot <goreleaser@carlosbecker.com>
2018-04-10 21:33:11 +1000

    Brew formula update for gocfn version 0.0.6

--- Formula/gocfn.rb
+++ Formula/gocfn.rb
@@ -1,9 +1,9 @@
 class Gocfn < Formula
   desc ""
   homepage ""
-  url "https://github.com/b-b3rn4rd/gocfn/releases/download/0.0.5/gocfn_0.0.5_darwin_amd64.tar.gz"
-  version "0.0.5"
-  sha256 "70f4778af9b8bad31c95aa8f8f6f431ebcab23293be5388d09503f22f0389761"
+  url "https://github.com/b-b3rn4rd/gocfn/releases/download/0.0.6/gocfn_0.0.6_darwin_amd64.tar.gz"
+  version "0.0.6"
+  sha256 "8917775ab39c09b7ed65a20518f9ecd7333ffdca5fe41d747149896b7003b708"
 
   def install
     bin.install "gocfn"
