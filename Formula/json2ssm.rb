86717fe22b640aa3922445b80e787b56286b7086
goreleaserbot <goreleaser@carlosbecker.com>
2018-05-18 09:58:16 +1000

    Brew formula update for json2ssm version 1.0.1

--- Formula/json2ssm.rb
+++ Formula/json2ssm.rb
@@ -1,9 +1,9 @@
 class Json2ssm < Formula
   desc ""
   homepage ""
-  url "https://github.com/b-b3rn4rd/json2ssm/releases/download/1.0.0/json2ssm_1.0.0_darwin_amd64.tar.gz"
-  version "1.0.0"
-  sha256 "f9e9757d4ba3f32090772fe0618cd38538677a9e512d8fc6b602056e00000df6"
+  url "https://github.com/b-b3rn4rd/json2ssm/releases/download/1.0.1/json2ssm_1.0.1_darwin_amd64.tar.gz"
+  version "1.0.1"
+  sha256 "6de58784f11db9449daa2b947b6db0c10899a22439a66d6151852833f931e6a6"
 
   def install
     bin.install "json2ssm"
